18cd7aec00e5d9f9528de6d295ea4e9d322deea3
Senthil A <senthil196@gmail.com>
2012-02-11 22:12:48 -0800

    Better design for Iteration specs

--- spec/linked_list/iteration_spec.rb
+++ spec/linked_list/iteration_spec.rb
@@ -1,55 +1,88 @@
-require 'spec_helper'
-require 'algorithm/linked_list/single_linked_list'
+require 'algorithm/linked_list'
 
-describe Algorithm::Iteration do
+[Algorithm::DoubleLinkedList, Algorithm::SingleLinkedList].each do |k|
 
-  # works with Algorithm::DoubleLinkedList as well
-  subject { Algorithm::SingleLinkedList.new }
+  describe k do
+    subject { k.new }
 
-  before do
-    subject << 'head'
-    subject << 'one'
-    subject << 'two'
-    subject << 'tail'
-  end
+    let(:elem_array) do
+      ['one', 'two', 'three', 'four', 'five']
+    end
 
-  it 'yields each node' do
-    [].tap do |arr|
-      subject.each_node do |x|
-        arr << x.data
+    let(:reverse_array) do
+      elem_array.reverse
+    end
+
+    before(:each) do
+      elem_array.each do |elem|
+        subject << elem
       end
-    end.should == ['tail', 'two', 'one', 'head']
-  end
+    end
 
-  it 'returns count' do
-    subject.count.should == 4
-  end
+    it 'yields each node' do
+      [].tap do |arr|
+        subject.each_node do |x|
+          arr << x.data
+        end
+      end.should == reverse_array
+    end
 
-  it 'yield data in each node' do
-    [].tap do |arr|
-      subject.each do |x|
-        arr << x
+    it 'returns count' do
+      subject.count.should == 5
+    end
+
+    it 'yield data in each node' do
+      [].tap do |arr|
+        subject.each do |x|
+          arr << x
+        end
+      end.should == reverse_array
+    end
+
+    it 'yields each node and collects result in an array' do
+      subject.map { |x| x}.should == reverse_array
+    end
+
+    it 'selects data which return true for block' do
+      subject.select { |x| x == 'three'}.first.should == 'three'
+    end
+
+    it 'returns data in an array' do
+      subject.to_a.should == reverse_array
+    end
+
+    it 'yields data with self increasing index' do
+      reverse_index_array = []
+      index               = 0
+
+      elem_array.reverse_each do |elem|
+        reverse_index_array << [elem, index]
+        index += 1
       end
-    end.should == ['tail', 'two', 'one', 'head']
-  end
 
-  it 'yields each node and collects result in an array' do
-    subject.map { |x| x}.should == ['tail', 'two', 'one', 'head']
-  end
+      [].tap do |arr|
+        subject.with_index do |data, index|
+          arr << [data, index]
+        end
+      end.should == reverse_index_array
+    end
 
-  it 'selects data which return true for block' do
-    subject.select { |x| x == 'tail'}.first.should == 'tail'
-  end
+    it 'finds data which match a single argument' do
+      subject.find('one').should == 'one'
+    end
 
-  it 'returns data in an array' do
-    subject.to_a.should == ['tail', 'two', 'one', 'head']
-  end
+    it 'finds data which match block' do
+      subject.find do |data|
+        data == 'one'
+      end.should == 'one'
+    end
 
-  it 'yields data with self increasing index' do
-    [].tap do |arr|
-      subject.with_index do |data, index|
-        arr << [data, index]
-      end
-    end.should == [["tail", 0], ["two", 1], ["one", 2], ["head", 3]]
+    it 'find data which match block and returns array if more than 1' do
+      subject << 'ones'
+
+      subject.find do |data|
+        data.include?('one')
+      end.should == ['ones', 'one']
+    end
   end
 end
