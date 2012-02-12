ea486214524afa68e6716f2e6ae46dde5f545e46
Senthil A <senthil196@gmail.com>
2012-02-11 22:11:44 -0800

    Double #push, #pop

--- lib/algorithm/linked_list/double_linked_list.rb
+++ lib/algorithm/linked_list/double_linked_list.rb
@@ -13,21 +13,77 @@ module Algorithm
   class DoubleLinkedList
     include Iteration
 
-    attr_reader :head, :tail
+    attr_reader :head, :tail, :size
+
+    def initialize
+      @size = 0
+    end
 
     def <<(data)
-      @tail = DoubleNode.new(:data => data, :tail => @tail, 
-                             :head => @head)
-      @head ||= @tail
+      node = DoubleNode.new(:data => data)
+
+      @size += 1
+
+      # list has head & tail, add to last
+      if @head && @tail
+        @tail.tail = node
+        node.head  = tail
+
+      # list has one node, add second node
+      elsif @head && @head.tail.nil?
+        @head.tail = node
+        node.head  = head
+
+      # list empty, add first node
+      elsif @tail.nil? && @head.nil?
+        @head = node
+      end
+
+      @tail = node
+
+      self
+    end
+
+    def pop
+      if @head && @tail
+
+        # list has only node
+        if @head == @tail
+          popped = @head
+
+          @head = nil
+          @tail = nil
+          @size -= 1
+
+          return popped.data
+
+        # list has multiple nodes
+        else
+          popped     = @tail
+          @tail      = @tail.head
+          @tail.tail = nil
+          @size -= 1
+
+          return popped.data
+        end
+      else
+
+        # list has no nodes
+        return nil
+      end
     end
 
     def previous
       @tail.tail
     end
+
+    def empty?
+      !@head && !@tail
+    end
   end
 
   class DoubleNode
-    attr_reader :data, :tail, :head
+    attr_accessor :data, :tail, :head
 
     def initialize(hash)
       @data = hash[:data]
--- spec/linked_list/double_linked_list_spec.rb
+++ spec/linked_list/double_linked_list_spec.rb
@@ -4,31 +4,213 @@ require 'algorithm/linked_list/double_linked_list'
 describe Algorithm::DoubleLinkedList do
   subject { described_class.new }
 
-  before do
-    subject << 'head'
-    subject << 'one'
-    subject << 'two'
-    subject << 'tail'
+  context 'zero nodes' do
+    it 'head is nil' do
+      subject.head.should == nil
+    end
+
+    it 'tail is nil' do
+      subject.tail.should == nil
+    end
+
+    it 'size is 0' do
+      subject.size.should == 0
+    end
+
+    it 'count is 0' do
+      subject.count.should == 0
+    end
+
+    it 'pops nothing' do
+      subject.pop.should == nil
+    end
+
+    it 'empty is true' do
+      subject.empty?.should == true
+    end
   end
 
-  it 'returns head' do
-    subject.head.data.should == 'head'
+  context 'one node' do
+    before do
+      subject << 'one'
+    end
+
+    it 'head points to first node added' do
+      subject.head.data.should == 'one'
+    end
+
+    it 'tail points to one nodel added' do
+      subject.tail.data.should == 'one'
+    end
+
+    it 'size is 1' do
+      subject.size.should == 1
+    end
+
+    it 'count is 1' do
+      subject.count.should == 1
+    end
+
+    it 'pop returns data' do
+      subject.pop.should == 'one'
+    end
+
+    it 'empty is false' do
+      subject.empty?.should == false
+    end
+
+    context '#pop' do
+      before do
+        subject.pop
+      end
+
+      it 'tail is nil' do
+        subject.tail.should == nil
+      end
+
+      it 'head is nil' do
+        subject.head.should == nil
+      end
+
+      it 'size is 0' do
+        subject.size.should == 0
+      end
+
+      it 'count is 0' do
+        subject.count.should == 0
+      end
+
+      it 'empty is false' do
+        subject.empty?.should == true
+      end
+    end
   end
 
-  it 'returns tail' do
-    subject.tail.data.should == 'tail'
+  context 'two nodes' do
+    before do
+      subject << 'one'
+      subject << 'two'
+    end
+
+    it 'head points to first node added' do
+      subject.head.data.should == 'one'
+    end
+
+    it 'tail points to last node added' do
+      subject.tail.data.should == 'two'
+    end
+
+    it 'size is 2' do
+      subject.size.should == 2
+    end
+
+    it 'head has no other heads' do
+      subject.head.head.should == nil
+    end
+
+    it 'head has one tail' do
+      subject.head.tail.data.should == 'two'
+    end
+
+    it 'tail has no other tails' do
+      subject.tail.tail.should == nil
+    end
+
+    it 'tail has one head' do
+      subject.tail.head.data.should == 'one'
+    end
+
+    it 'pop returns data' do
+      subject.pop.should == 'two'
+    end
+
+    context '#pop' do
+      before do
+        subject.pop
+      end
+
+      it 'moves tail to one before popped node' do
+        subject.tail.data.should == 'one'
+      end
+
+      it 'head remains the same' do
+        subject.head.data.should == 'one'
+      end
+
+      it 'size is 1' do
+        subject.size.should == 1
+      end
+    end
   end
 
-  it do
-    subject.previous.data.should == 'two'
-  end
+  context 'multiple nodes' do
+    before do
+      subject << 'one'
+      subject << 'two'
+      subject << 'three'
+      subject << 'four'
+      subject << 'five'
+    end
 
-  it do
-    subject.head.tail.should == nil
-  end
+    it 'head points to first node added' do
+      subject.head.data.should == 'one'
+    end
 
-  it do
-    subject.tail.tail.data.should == 'two'
+    it 'tail points to last node added' do
+      subject.tail.data.should == 'five'
+    end
+
+    it 'size is 5' do
+      subject.size.should == 5
+    end
+
+    it 'head has no other heads' do
+      subject.head.head.should == nil
+    end
+
+    it 'head has one tail' do
+      subject.head.tail.data.should == 'two'
+    end
+
+    it 'tail has no other tails' do
+      subject.tail.tail.should == nil
+    end
+
+    it 'tail has one head' do
+      subject.tail.head.data.should == 'four'
+    end
+
+    it 'traverses all the way to head' do
+      subject.tail.head.head.data.should           == 'three'
+      subject.tail.head.head.head.data.should      == 'two'
+      subject.tail.head.head.head.head.data.should == 'one'
+    end
+
+    it 'pop returns data' do
+      subject.pop.should == 'five'
+    end
+
+    context '#pop' do
+      before do
+        subject.pop
+      end
+
+      it 'moves tail to one before popped node' do
+        subject.tail.data.should == 'four'
+      end
+
+      it 'new tail has no other tails' do
+        subject.tail.tail.should == nil
+      end
+
+      it 'head remains the same' do
+        subject.head.data.should == 'one'
+      end
+
+      it 'size is 4' do
+        subject.size.should == 4
+      end
+    end
   end
 end
 
