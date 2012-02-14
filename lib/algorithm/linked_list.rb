a3f60506c9b3d7eeba01f365f2f7f435ccab8f1d
Senthil A <senthil196@gmail.com>
2012-02-13 20:08:41 -0800

    req IterationReverse in LinkedList

--- lib/algorithm/linked_list.rb
+++ lib/algorithm/linked_list.rb
@@ -1,3 +1,4 @@
 require_relative 'linked_list/iteration'
+require_relative 'linked_list/iteration_reverse'
 require_relative 'linked_list/single_linked_list'
 require_relative 'linked_list/double_linked_list'
